96bdd96030181555aab3090457aa903ccf1830b9
seldacetin <64745949+seldacetin@users.noreply.github.com>
2023-04-07 13:53:05 +0300

    python_calismalari

--- django/MovieApp/MovieApp/wsgi.py
+++ django/MovieApp/MovieApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sinemadb project.
+WSGI config for MovieApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sinemadb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MovieApp.settings')
 
 application = get_wsgi_application()
