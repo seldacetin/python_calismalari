dcc047b301a36df16a697e2cd8606239fac358aa
seldacetin <64745949+seldacetin@users.noreply.github.com>
2023-04-19 15:38:45 +0300

    python_calismalari

--- django/MovieApp/movies/models.py
+++ django/MovieApp/movies/models.py
@@ -7,3 +7,6 @@ class Movie(models.Model):
     film_adi = models.CharField(max_length=200)
     aciklama = models.TextField()
     resim = models.CharField(max_length=100)
+
+def __str__(self):
+    return f"{self.film_adi}"
