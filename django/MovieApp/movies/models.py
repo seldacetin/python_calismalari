ff09676af4115367115d4b023beb3915dd19b1fa
seldacetin <64745949+seldacetin@users.noreply.github.com>
2023-04-24 15:38:07 +0300

    python_calismalari

--- django/MovieApp/movies/models.py
+++ django/MovieApp/movies/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length = 100)
 
@@ -7,6 +8,7 @@ class Movie(models.Model):
     film_adi = models.CharField(max_length=200)
     aciklama = models.TextField()
     resim = models.CharField(max_length=100)
+    anasayfa = models.BooleanField(default=False)
 
 def __str__(self):
     return f"{self.film_adi}"
